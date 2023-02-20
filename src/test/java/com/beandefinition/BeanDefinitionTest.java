ac7545d11e5f02e146c5daba38e108ec619c0710
sponbob-pat <ixdos1111@gmail.com>
2023-02-20 10:51:51 +0900

    BeanDefiniton에 대해

---
+++ src/test/java/com/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,44 @@
+package com.beandefinition;
+
+import com.springCore.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    // BeanDefinition의 내용을 직접 출력해서 확인해보기
+    @Test
+    @DisplayName("빈 설정 메타 정보 확인")
+    void findApplicationBean() {
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for(String beanDefinitionName : beanDefinitionNames){
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName
+                        + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+    // 출력 내용
+    /*
+    beanDefinitionName = appConfig
+    beanDefinition = Generic bean: class [com.springCore.AppConfig$$EnhancerBySpringCGLIB$$c64b2bc3]; scope=singleton; abstract=false; lazyInit=null; autowireMode=0; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=null; factoryMethodName=null; initMethodName=null; destroyMethodName=null
+
+    beanDefinitionName = memberRepository beanDefinition = Root bean: class [null]; scope=; abstract=false; lazyInit=null; autowireMode=3; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=appConfig; factoryMethodName=memberRepository; initMethodName=null; destroyMethodName=(inferred); defined in com.springCore.AppConfig
+    beanDefinitionName = memberService beanDefinition = Root bean: class [null]; scope=; abstract=false; lazyInit=null; autowireMode=3; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=appConfig; factoryMethodName=memberService; initMethodName=null; destroyMethodName=(inferred); defined in com.springCore.AppConfig
+    beanDefinitionName = discountPolicy beanDefinition = Root bean: class [null]; scope=; abstract=false; lazyInit=null; autowireMode=3; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=appConfig; factoryMethodName=discountPolicy; initMethodName=null; destroyMethodName=(inferred); defined in com.springCore.AppConfig
+    beanDefinitionName = orderService beanDefinition = Root bean: class [null]; scope=; abstract=false; lazyInit=null; autowireMode=3; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=appConfig; factoryMethodName=orderService; initMethodName=null; destroyMethodName=(inferred); defined in com.springCore.AppConfig
+
+
+
+     */
+
+}
