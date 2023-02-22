4ca727230c6f9855e0751879a584637e9fce975c
sponbob-pat <ixdos1111@gmail.com>
2023-02-22 18:07:58 +0900

    스코프와 Provider

--- src/main/java/com/springCore/web/LogDemoController.java
+++ src/main/java/com/springCore/web/LogDemoController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.springCore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody // 반환되는 문자를 그대로 화면에 출력하도록 함
     public String logDemo(HttpServletRequest request) {
+
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
