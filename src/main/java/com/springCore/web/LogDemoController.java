907ef6a375e9e92d1e5f7f03a46076a255b8ae9a
sponbob-pat <ixdos1111@gmail.com>
2023-02-22 23:13:38 +0900

    스코프와 프록시

--- src/main/java/com/springCore/web/LogDemoController.java
+++ src/main/java/com/springCore/web/LogDemoController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.springCore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +13,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody // 반환되는 문자를 그대로 화면에 출력하도록 함
     public String logDemo(HttpServletRequest request) {
 
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        // 출력 내용
+        // myLogger = class com.springCore.common.MyLogger$$EnhancerBySpringCGLIB$$e8e206c4
+
+        // 가짜 mylogger라는 걸 알 수 있음. CGLIB를 이용해서 가짜 프록시 객체를 만들어서 주입함
+
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
